b85426b7c688dbab2c899ec6da45ea3d81ffff28
Clement Helsens <clementhelsens>
2023-10-09 14:54:53 +0200

    adding project template

--- experiment_catalog/urls.py
+++ experiment_catalog/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 
 urlpatterns = [
  
-
-
     path('experiments/', views.ExperimentListView.as_view(), name='experiments'),
     path('experiment/<int:pk>', views.ExperimentDetailView.as_view(), name='experiment-detail'),
 
--- project_catalog/urls.py
+++ project_catalog/urls.py
@@ -8,13 +8,3 @@ urlpatterns = [
 ]
 
 
-# Add URLConf to create, update, and delete books
-urlpatterns += [
-#    path('book/create/', views.BookCreate.as_view(), name='book-create'),
-#    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
-#    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
-
-    #path('project/create/', views.ProjectCreate.as_view(), name='project-create'),
-    path('project/create/', views.ProjectCreate, name='project-create'),
-
-]
