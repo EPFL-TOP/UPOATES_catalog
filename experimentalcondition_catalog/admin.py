5d8a1b5ee109a5f321de051a730fd91a7ff5e881
Clement Helsens <clementhelsens>
2024-05-06 14:13:58 +0200

    test sample

--- experimentalcondition_catalog/admin.py
+++ experimentalcondition_catalog/admin.py
@@ -10,13 +10,18 @@ class ExperimentalConditionAdmin(admin.ModelAdmin):
     search_fields = ["experimentaldataset__raw_dataset__data_type","experimentaldataset__raw_dataset__data_name"]
     #autocomplete_fields  = ["experimentaldataset__raw_dataset__data_type","experimentaldataset__raw_dataset__data_name"]
     
+class SampleAdmin(admin.ModelAdmin):
+    search_fields = ["experimentalcondition__experimentaldataset__raw_dataset__data_name"]
+#        expds =  ", ".join("("+str(par.experimentaldataset.raw_dataset.data_name)+")" for par in self.experimentalcondition_set.all())
+
+
 admin.site.register(Parent)
 admin.site.register(Mutation)
 admin.site.register(MutationGrade)
 admin.site.register(MutationName)
 admin.site.register(Treatment)
 admin.site.register(Injection)
-admin.site.register(Sample)
+admin.site.register(Sample, SampleAdmin)
 admin.site.register(ExperimentalCondition, ExperimentalConditionAdmin)
 admin.site.register(InstrumentalCondition)
 admin.site.register(Fixation)
